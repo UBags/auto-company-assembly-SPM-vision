68e62adf3d94f964cbf8e366f2099cf1010c92fb
UBags <67504653+UBags@users.noreply.github.com>
2026-03-23 03:48:55 +0530

    Update BaseUtils.py

--- BaseUtils.py
+++ BaseUtils.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Uddipan Bagchi. All rights reserved.
+# See LICENSE in the project root for license information.package com.costheta.cortexa.action
+
 # VERY, VERY IMPORTANT NOTE: Ensure that BaseUtils is in the project root.
 # This is to ensure that getFullyQualifiedName works properly
 
