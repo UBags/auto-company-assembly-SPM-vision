ff64a20df4c030069a38a2c925f27bd7173880e9
UBags <67504653+UBags@users.noreply.github.com>
2026-03-23 03:35:21 +0530

    Update CheckCap.py

--- camera/CheckCap.py
+++ camera/CheckCap.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Uddipan Bagchi. All rights reserved.
+# See LICENSE in the project root for license information.package com.costheta.cortexa.action
+
 """
 CheckCap - Detects presence of Cap by comparing with previous assembly stage images.
 
