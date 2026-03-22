83f31a1aebaabe2b092d0acdf36676a2b5dc3007
UBags <67504653+UBags@users.noreply.github.com>
2026-03-23 03:37:00 +0530

    Update CheckTopBearing.py

--- camera/CheckTopBearing.py
+++ camera/CheckTopBearing.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Uddipan Bagchi. All rights reserved.
+# See LICENSE in the project root for license information.package com.costheta.cortexa.action
+
 """
 CheckTopBearing v2.0 - Integrated with TopBearingSegmentation Pipeline
 
